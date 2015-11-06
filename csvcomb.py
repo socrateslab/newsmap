c637898d645f95ae0e1303821e1b2feb69aae2da
刘磊 <kira.36@live.cn>
2015-11-06 20:44:14 +0800

    Update csvcomb.py

--- csvcomb.py
+++ csvcomb.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-for i in os.walk('files'):
+for i in os.walk('getData'):
     filenames = i[2]
 
 numOfFiles = len(filenames)
@@ -10,9 +10,9 @@ files = [0 for i in range(numOfFiles)]
 for i in range(1,numOfFiles):
 	#in mac OS X , from "1" because of DS.store ,in linux or win ,use from "0"
     print filenames[i]
-    fileAddress = "files/"+filenames[i]
+    fileAddress = "getData/"+filenames[i]
     files[i] = pd.read_csv(fileAddress)
 for i in range(2,numOfFiles):
     files[1] = files[1].merge(files[i], on="NAME")
-files[1].to_csv("provinces_news.csv",index=False)
+files[1].to_csv("data/provinces_news.csv",index=False)
 
