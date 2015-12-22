71da7a664bb60bf5fa675cb0decdcc5a85fb4c0e
Kelsiii <wdjwjzjy@163.com>
2015-12-22 20:32:30 +0800

    南北地理分布&增加省市

--- admin/constants.php
+++ admin/constants.php
@@ -1,5 +1,5 @@
 <?php
-	define("dbServer","127.0.0.1");		//数据库服务器
+	define("dbServer","localhost");		//数据库服务器
 	define("dbUserName","root");	//数据库用户
 	define("dbPassword","");		//数据库登陆密码
 	define("database","test");		//数据库名称
--- admin/getdata.php
+++ admin/getdata.php
@@ -6,6 +6,294 @@
         setYearChina();
     if($type=='weekline')
         setRecentWeekChina();
+    if($type=='monthpie')
+        setLastMonthChina();
+    if($type=='dot')
+        setAllChina();
+    
+    function setRecentMonthChina(){
+        
+    }
+
+    function setAllChina(){
+        if(($con=mysql_connect(dbServer,dbUserName,dbPassword))===FALSE)
+            die("could not connect to database");
+        //验证服务器连接
+        if(mysql_select_db(database)===FALSE)
+            die("could not connect to database");
+        
+        $sql="Select ActionGeo_ADM1Code as province,count(*) as newsnum from chinadata where ActionGeo_CountryCode='CH' group by ActionGeo_ADM1Code union Select ActionGeo_CountryCode as province,count(*) as newsnum from chinadata where (ActionGeo_CountryCode='MC' or ActionGeo_CountryCode='HK' or ActionGeo_CountryCode='TW') group by ActionGeo_CountryCode
+";
+        $result = mysql_query($sql);
+        if($result===FALSE)
+            die("could not query database");
+        while($row = mysql_fetch_array($result)){
+            switch($row['province']){
+                case 'CH01':
+                    $anhui=$row['newsnum'];
+                    break;
+                case 'CH02':
+                    $zhejiang=$row['newsnum'];
+                    break;
+                case 'CH03':
+                    $jiangxi=$row['newsnum'];
+                    break;
+                case 'CH04':
+                    $jiangsu=$row['newsnum'];
+                    break;
+                case 'CH05':
+                    $jilin=$row['newsnum'];
+                    break;
+                case 'CH06':
+                    $qinghai=$row['newsnum'];
+                    break;
+                case 'CH07':
+                    $fujian=$row['newsnum'];
+                    break;
+                case 'CH08':
+                    $heilongjiang=$row['newsnum'];
+                    break;
+                case 'CH09':
+                    $henan=$row['newsnum'];
+                    break;
+                case 'CH10':
+                    $hebei=$row['newsnum'];
+                    break;
+                case 'CH11':
+                    $hunan=$row['newsnum'];
+                    break;
+                case 'CH12':
+                    $hubei=$row['newsnum'];
+                    break;
+                case 'CH13':
+                    $xinjiang=$row['newsnum'];
+                    break;
+                case 'CH14':
+                    $xizang=$row['newsnum'];
+                    break;
+                case 'CH15':
+                    $gansu=$row['newsnum'];
+                    break;
+                case 'CH16':
+                    $guangxi=$row['newsnum'];
+                    break;
+                case 'CH18':
+                    $guizhou=$row['newsnum'];
+                    break;
+                case 'CH19':
+                    $liaoning=$row['newsnum'];
+                    break;
+                case 'CH20':
+                    $neimeng=$row['newsnum'];
+                    break;
+                case 'CH21':
+                    $ningxia=$row['newsnum'];
+                    break;
+                case 'CH22':
+                    $beijing=$row['newsnum'];
+                    break;
+                case 'CH23':
+                    $shanghai=$row['newsnum'];
+                    break;
+                case 'CH24':
+                    $shanxi=$row['newsnum'];
+                    break;
+                case 'CH25':
+                    $shandong=$row['newsnum'];
+                    break;
+                case 'CH26':
+                    $shaanxi=$row['newsnum'];
+                    break;
+                case 'CH28':
+                    $tianjin=$row['newsnum'];
+                    break;
+                case 'CH29':
+                    $yunnan=$row['newsnum'];
+                    break;
+                case 'CH30':
+                    $guangdong=$row['newsnum'];
+                    break;
+                case 'CH31':
+                    $hainan=$row['newsnum'];
+                    break;
+                case 'CH32':
+                    $sichuan=$row['newsnum'];
+                    break;
+                case 'CH33':
+                    $chongqing=$row['newsnum'];
+                    break;
+                case 'HK':
+                    $hongkong=$row['newsnum'];
+                    break;
+                case 'MC':
+                    $macau=$row['newsnum'];
+                    break;
+                case 'TW':
+                    $taiwan=$row['newsnum'];
+                    break;
+            }
+        }
+        $return=array("beijing"=>$beijing,"tianjin"=>$tianjin,"chongqing"=>$chongqing,"shanghai"=>$shanghai,"hebei"=>$hebei,"shanxi"=>$shanxi,"liaoning"=>$liaoning,"jilin"=>$jilin,"heilongjiang"=>$heilongjiang,"jiangsu"=>$jiangsu,"zhejiang"=>$zhejiang,"anhui"=>$anhui,"fujian"=>$fujian,"jiangxi"=>$jiangxi,"shandong"=>$shandong,"henan"=>$henan,"hubei"=>$hubei,"hunan"=>$hunan,"guangdong"=>$guangdong,"hainan"=>$hainan,"sichuan"=>$sichuan,"guizhou"=>$guizhou,"yunnan"=>$yunnan,"shaanxi"=>$shaanxi,"gansu"=>$gansu,"qinghai"=>$qinghai,"taiwan"=>$taiwan,"neimeng"=>$neimeng,"guangxi"=>$guangxi,"xizang"=>$xizang,"ningxia"=>$ningxia,"xinjiang"=>$xinjiang,"hongkong"=>$hongkong,"macau"=>$macau);
+        
+        foreach ( $return as $key => $value ) {  
+            $return[$key] = urlencode ( $value );  
+        }
+        echo urldecode(json_encode($return));
+    }
+
+    function setLastMonthChina(){
+        if(($con=mysql_connect(dbServer,dbUserName,dbPassword))===FALSE)
+            die("could not connect to database");
+        //验证服务器连接
+        if(mysql_select_db(database)===FALSE)
+            die("could not connect to database");
+        $data="";
+        $province="";
+        $i=0;
+        $sum=0;
+        $year = date('Y',time());
+        $month = date('m',time());
+        $day="01";
+        $beginDate=$_date = date("Ymd",mktime(0,0,0,$month,$day,$year));
+        $endDate = date('Ymd', strtotime("$beginDate +1 month"));
+        $sql="Select ActionGeo_ADM1Code as province,count(*) as newsnum from chinadata where ActionGeo_CountryCode='CH' and DATEADDED>='$beginDate' and DATEADDED<'$endDate' group by ActionGeo_ADM1Code union Select ActionGeo_CountryCode as province,count(*) as newsnum from chinadata where (ActionGeo_CountryCode='MC' or ActionGeo_CountryCode='HK' or ActionGeo_CountryCode='TW') and DATEADDED>='$beginDate' and DATEADDED<'$endDate' group by ActionGeo_CountryCode order by newsnum desc
+";
+        $result = mysql_query($sql);
+        if($result===FALSE)
+            die("could not query database");
+        while($row = mysql_fetch_array($result)){
+            $sum = $sum+$row['newsnum'];
+            if($i<5){
+                if($row['province']=="CH"||$row['province']=="CH00")
+                    continue;
+                else{
+                    $pro="";
+                    switch($row['province']){
+                        case 'CH01':
+                            $pro="安徽";
+                            break;
+                        case 'CH02':
+                            $pro="浙江";
+                            break;
+                        case 'CH03':
+                            $pro="江西";
+                            break;
+                        case 'CH04':
+                            $pro="江苏";
+                            break;
+                        case 'CH05':
+                            $pro="吉林";
+                            break;
+                        case 'CH06':
+                            $pro="青海";
+                            break;
+                        case 'CH07':
+                            $pro="福建";
+                            break;
+                        case 'CH08':
+                            $pro="黑龙江";
+                            break;
+                        case 'CH09':
+                            $pro="河南";
+                            break;
+                        case 'CH10':
+                            $pro="河北";
+                            break;
+                        case 'CH11':
+                            $pro="湖南";
+                            break;
+                        case 'CH12':
+                            $pro="湖北";
+                            break;
+                        case 'CH13':
+                            $pro="新疆";
+                            break;
+                        case 'CH14':
+                            $pro="西藏";
+                            break;
+                        case 'CH15':
+                            $pro="甘肃";
+                            break;
+                        case 'CH16':
+                            $pro="广西";
+                            break;
+                        case 'CH18':
+                            $pro="贵州";
+                            $guizhou=$guizhou.$row['newsnum'];
+                            break;
+                        case 'CH19':
+                            $pro="辽宁";
+                            break;
+                        case 'CH20':
+                            $pro="内蒙古";
+                            break;
+                        case 'CH21':
+                            $pro="宁夏";
+                            break;
+                        case 'CH22':
+                            $pro="北京";
+                            break;
+                        case 'CH23':
+                            $pro="上海";
+                            break;
+                        case 'CH24':
+                            $pro="山西";
+                            break;
+                        case 'CH25':
+                            $pro="山东";
+                            break;
+                        case 'CH26':
+                            $pro="陕西";
+                            break;
+                        case 'CH28':
+                            $pro="天津";
+                            break;
+                        case 'CH29':
+                            $pro="云南";
+                            break;
+                        case 'CH30':
+                            $pro="广东";
+                            break;
+                        case 'CH31':
+                            $pro="海南";
+                            break;
+                        case 'CH32':
+                            $pro="四川";
+                            break;
+                        case 'CH33':
+                            $pro="重庆";
+                            break;
+                        case 'HK':
+                            $pro="香港";
+                            break;
+                        case 'MC':
+                            $pro="澳门";
+                            break;
+                        case 'TW':
+                            $pro="台湾";
+                            break;
+                    }
+                    $province=$province.$pro;
+                    $data=$data.$row['newsnum'];
+                    $i=$i+1;
+
+                    if($i<5){
+                        $data=$data.",";
+                        $province=$province.",";
+                    }
+                }
+            }
+        }
+        $return=array("sum"=>$sum,"data"=>$data,"province"=>$province);
+        
+        foreach ( $return as $key => $value ) {  
+            $return[$key] = urlencode ( $value );  
+        }
+        echo urldecode(json_encode($return));
+        
+        
+    }
+
 
     function setRecentWeekChina(){
          if(($con=mysql_connect(dbServer,dbUserName,dbPassword))===FALSE)
@@ -73,7 +361,7 @@
         //循环查询每月新闻数
         $beijing="";$tianjin="";$chongqing="";$shanghai="";$hebei="";$shanxi="";$liaoning="";$jilin="";$heilongjiang="";$jiangsu = "";$zhejiang="";$anhui="";$fujian="";$jiangxi="";$shandong="";$henan="";$hubei="";$hunan="";$guangdong="";$hainan="";$sichuan="";$guizhou="";$yunnan="";$shaanxi="";$gansu="";$qinghai="";$taiwan="";$neimeng="";$guangxi="";$xizang="";$ningxia="";$xinjiang="";$hongkong="";$macau="";
         for($i=1;$i<13;$i++){
-            $year = date('Y',time());
+            $year = '2015';
             if($i>1){
                 $beijing=$beijing.",";
                 $tianjin=$tianjin.",";
@@ -141,7 +429,7 @@
                     case 'CH05':
                         $jilin=$jilin.$row['newsnum'];
                         break;
-                    case ' CH06':
+                    case 'CH06':
                         $qinghai=$qinghai.$row['newsnum'];
                         break;
                     case 'CH07':
