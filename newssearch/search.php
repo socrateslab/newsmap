4e62298f255c2e98032049c55ffa3b5236d776c2
Kelsiii <wdjwjzjy@163.com>
2016-04-27 14:10:03 +0800

    搜索分页

--- newssearch/search.php
+++ newssearch/search.php
@@ -2,16 +2,19 @@
 require_once('vendor/autoload.php');  
 
 $keyword = $_GET['word'];
+$page = $_GET['page'];
 //echo $keyword;
 
-search($keyword);
+search($keyword,$page);
 
-function search($word){  
+function search($word,$page){  
     //Elastic search php client  
     $client = new Elasticsearch\Client();  
     $params = array();  
     $params['index'] = 'news';  
     $params['type'] = 'news_info';  
+    $params['from'] = $page*30;
+    $params['size'] = 30;
     $params['body']['query']['match']['title'] = $word;  
   
     $rtn = $client->search($params);  
