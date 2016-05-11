e99caaaf93d79a2ab1ce29dd711846bca899d806
Kelsiii <wdjwjzjy@163.com>
2016-05-11 14:00:34 +0800

    香港搜索

--- search/search.php
+++ search/search.php
@@ -3,9 +3,41 @@ require_once('vendor/autoload.php');
 
 $keyword = $_GET['word'];
 $page = $_GET['page'];
+$type = $_GET['type'];
 //echo $keyword;
 
-search($keyword,$page);
+if($type=="cn")
+    search($keyword,$page);
+if($type=="hk")
+    searchhk($keyword,$page);
+
+function searchhk($word,$page){  
+    //Elastic search php client  
+    $client = new Elasticsearch\Client();  
+    $params = array();  
+    $params['index'] = 'news_hk';  
+    $params['type'] = 'news_info';  
+    $params['from'] = $page*30;
+    $params['size'] = 30;
+    $params['body']['query']['match']['title'] = $word;  
+  
+    $rtn = $client->search($params);  
+    $list=$rtn["hits"]["hits"];
+    
+    $output = [];
+    $i=0;
+    foreach ( $list as $key => $value ) {  
+        $info=$list[$key]["_source"];
+        foreach ( $info as $key => $value ) {  
+            $info[$key] = urlencode ( str_replace(array("\r\n", "\r", "\n"), "", $value) );  
+        } 
+        
+        $output[$i] = $info;
+        $i = $i+1;
+    }
+    echo urldecode(json_encode($output));
+    
+} 
 
 function search($word,$page){  
     //Elastic search php client  
